c713d92bf8cf3c2b876c6ae4163b4b276749f601
krishkrithi97 <krithikukku@gmail.com>
2021-03-05 18:09:20 +0530

    second commit

--- krithi-jobs-plugin.php
+++ krithi-jobs-plugin.php
@@ -1,186 +1,289 @@
-<?php
-/**
-* @package PoemhdPlugin
-*/
-/*
- * Plugin Name:       Jobs Plugin
- * Plugin URI:        https://example.com/plugins/the-basics/
- * Description:       Handle the basics with this plugin.
- * Version:           1.10.3
- * Requires at least: 5.2
- * Requires PHP:      7.2
- * Author:            Krithi Krishna
- * Author URI:        https://author.example.com/
- * License:           GPL v2 or later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       custom-jobs-plugin
- * Domain Path:       /languages
- */
- /*
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version 2
- of the License, or (at your option) any later version.
+  <?php
+  /**
+  * @package PoemhdPlugin
+  */
+  /*
+   * Plugin Name:       Jobs Plugin
+   * Plugin URI:        https://example.com/plugins/the-basics/
+   * Description:       Handle the basics with this plugin.
+   * Version:           1.10.3
+   * Requires at least: 5.2
+   * Requires PHP:      7.2
+   * Author:            Krithi Krishna
+   * Author URI:        https://author.example.com/
+   * License:           GPL v2 or later
+   * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+   * Text Domain:       custom-jobs-plugin
+   * Domain Path:       /languages
+   */
+   /*
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
- Copyright 2005-2015 Automattic, Inc.
- */
- defined( 'ABSPATH' ) or die('Hey,you can\t access this file!' );
-
- function add_custom_meta_box()
- {
-     add_meta_box("demo-meta-box", "Custom Meta Box", "custom_meta_box_markup", "jobs", "side", "high", null);
- }
-
- add_action("add_meta_boxes", "add_custom_meta_box");
-
- function custom_meta_box_markup($object)
-{
-    wp_nonce_field(basename(__FILE__), "meta-box-nonce");
-
-       // get previously saved meta values (if any)
-       $event_start_date = get_post_meta( $object->ID, 'event-start-date', true );
-       $event_end_date = get_post_meta( $object->ID, 'event-end-date', true );
-       $event_venue = get_post_meta( $object->ID, 'event-venue', true );
-
-       // if there is previously saved value then retrieve it, else set it to the current time
-       $event_start_date = ! empty( $event_start_date ) ? $event_start_date : time();
-
-       //we assume that if the end date is not present, event ends on the same day
-       $event_end_date = ! empty( $event_end_date ) ? $event_end_date : $event_start_date;
+   Copyright 2005-2015 Automattic, Inc.
+   */
+   defined( 'ABSPATH' ) or die('Hey,you can\t access this file!' );
 
 
-    ?>
-        <div>
-            <label for="meta-box-text">Title</label>
-            <input name="meta-box-text" type="text" value="<?php echo get_post_meta($object->ID, "meta-box-text", true); ?>">
+  if( !class_exists( 'CutomJobsPlugin' ) ) {
 
-            <br>
-
-            <label for="uep-event-start-date"><?php _e( 'Event Start Date:', 'demo-meta-box' ); ?></label>
-                    <input class="widefat uep-event-date-input" id="uep-event-start-date" type="text" name="uep-event-start-date" placeholder="Format: February 18, 2014" value="<?php echo date( 'F d, Y', $event_start_date ); ?>" />
-
-            <label for="uep-event-end-date"><?php _e( 'Event End Date:', 'demo-meta-box' ); ?></label>
-                    <input class="widefat uep-event-date-input" id="uep-event-end-date" type="text" name="uep-event-end-date" placeholder="Format: February 18, 2014" value="<?php echo date( 'F d, Y', $event_end_date ); ?>" />
-
-            <br><br>
-            <label for="meta-box-text">Email</label>
-            <input name="meta-box-text" type="email" value="<?php echo get_post_meta($object->ID, "meta-box-email", true); ?>">
-
-
-
-        </div>
-    <?php
-}
-
-function save_custom_meta_box($post_id, $post, $update)
-{
-    if (!isset($_POST["meta-box-nonce"]) || !wp_verify_nonce($_POST["meta-box-nonce"], basename(__FILE__)))
-        return $post_id;
-
-    if(!current_user_can("edit_post", $post_id))
-        return $post_id;
-
-    if(defined("DOING_AUTOSAVE") && DOING_AUTOSAVE)
-        return $post_id;
-
-    $slug = "post";
-    if($slug != $post->post_type)
-        return $post_id;
-
-    $meta_box_text_value = "";
-    $meta_box_start_date_value = "";
-    $meta_box_end_date_value = "";
-    $meta_box_email_value = "";
-
-
-    if(isset($_POST["meta-box-text"]))
+    class CutomJobsPlugin
     {
-        $meta_box_text_value = $_POST["meta-box-text"];
+
+      function __construct() {
+        add_action( 'init', array( $this,'custom_post_type') );
+        add_action( 'add_meta_boxes', array( $this,'jobs_add_expiry_date_metabox') );
+        add_action( 'save_post', array( $this,'jobs_save_expiry_date_meta' ));
+        add_action( 'add_meta_boxes', array( $this,'jobs_add_metabox') );
+        add_action( 'save_post', array( $this,'save_jobs_meta' ));
+        add_action( 'pre_get_posts', array( $this,'jobs_filter_expired_posts' ));
+      }
+
+      function register() {
+      add_action( 'admin_enqueue_scripts', array( $this,'jobs_load_jquery_datepicker') );
+      }
+
+      // function tutsplus_add_expiry_date_metabox() {
+      //   add_meta_box( 'expiry-date','Custom Expiry Date',array($this,'tutsplus_expiry_date_metabox_callback'),'jobs','side','core');
+      // }
+      function jobs_add_expiry_date_metabox() {
+          add_meta_box(
+              'jobs_expiry_date_metabox',
+              __( 'Custom Expiry Date', 'jobexp'),
+              array($this,'jobs_expiry_date_metabox_callback'),
+              'jobs',
+              'side',
+              'core'
+          );
+      }
+
+      function jobs_expiry_date_metabox_callback( $post ) {
+      ?>
+
+        <form action="" method="post">
+
+            <?php
+            // add nonce for security
+            wp_nonce_field( 'jobs_expiry_date_metabox_nonce', 'jobs_nonce' );
+
+            //retrieve metadata value if it exists
+            $jobs_expiry_date = get_post_meta( $post->ID, 'expires', true );
+            ?>
+
+            <label for "jobs_expiry_date"><?php ('Expiry Date'); ?></label>
+
+            <input type="text" class="MyDate" name="jobs_expiry_date" value=<?php echo esc_attr( $jobs_expiry_date ); ?> >
+
+            <script type="text/javascript">
+                jQuery(document).ready(function() {
+                    jQuery('.MyDate').datepicker({
+                        dateFormat : 'dd-mm-yy'
+                    });
+                });
+            </script>
+
+        </form>
+
+    <?php }
+    function jobs_save_expiry_date_meta( $post_id ) {
+        // write_log(test);
+        // Check if the current user has permission to edit the post. */
+        if ( !current_user_can( 'edit_post', $post_id->ID ) )
+        return;
+
+        if ( isset( $_POST['jobs_expiry_date'] ) ) {
+            $new_expiry_date = ( $_POST['jobs_expiry_date'] );
+            update_post_meta( $post_id, 'expires', $new_expiry_date );
+        }
+
     }
-    update_post_meta($post_id, "meta-box-text", $meta_box_text_value);
 
 
-    if(isset($_POST["uep-event-start-date"]))
-    {
-        $meta_box_start_date_value = $_POST["uep-event-start-date"];
-    }
-    update_post_meta($post_id, "uep-event-start-date", $meta_box_start_date_value);
+    function jobs_filter_expired_posts( $query ) {
 
+      // doesn't affect admin screens
+      if ( is_admin() )
+          return;
+      // check for main query
+      if ( $query->is_main_query() ) {
 
-    if(isset($_POST["uep-event-end-date"]))
-    {
-        $meta_box_end_date_value = $_POST["uep-event-end-date"];
-    }
-    update_post_meta($post_id, "uep-event-end-date", $meta_box_end_date_value);
-
-
-    if(isset($_POST["meta-box-email"]))
-    {
-        $meta_box_email_value = $_POST["meta-box-email"];
-    }
-    update_post_meta($post_id, "meta-box-email", $meta_box_email_value);
-
-}
-
-add_action("save_post", "save_custom_meta_box", 10, 3);
-
-
-
-
-if( !class_exists( 'CutomJobsPlugin' ) ) {
-
-  class CutomJobsPlugin
-  {
-
-    function __construct() {
-      add_action( 'init', array( $this,'custom_post_type') );
+          //filter out expired posts
+          $today = date('d-m-Y');
+          $metaquery = array(
+              array(
+                   'key' => 'expires',
+                   'value' => $today,
+                   'compare' => '<',
+                   'type' => 'DATE',
+              )
+          );
+          $query->set( 'meta_query', $metaquery );
+      }
     }
 
-    function register() {
-      add_action( 'admin_enqueue_scripts', array( $this,'enqueue') );
+
+    function jobs_add_metabox() {
+        add_meta_box(
+            'jobs_metabox',
+            __( 'Custom Meta Box', 'jobs-meta'),
+            array($this,'jobs_metabox_callback'),
+            'jobs',
+            'side',
+            'core',
+        );
     }
 
-    function activate() {
-      $this->custom_post_type();
-      flush_rewrite_rules();
+    function jobs_metabox_callback( $post ) {
+
+        // Add a nonce field so we can check for it later.
+        wp_nonce_field( 'global_notice_nonce', 'global_notice_nonce' );
+
+        $value1 = get_post_meta( $post->ID, '_title-meta', true );
+        $value2 = get_post_meta( $post->ID, '_email-meta', true );
+        ?>
+        <form action="" method="POST" enctype="multipart/form-data" name="form">
+          <label for="meta-box-title">Title</label>
+          <input type="text" name="title-meta" value="<?php echo esc_html( $value1 ); ?>">
+          <br>
+          <br>
+
+          <label for="meta-box-email">Email</label>
+          <input type="email" name="email-meta" value="<?php echo esc_html( $value2 ); ?>">
+  <?php
     }
 
-    function deactivate() {
-      flush_rewrite_rules();
+
+
+
+    /**
+     * When the post is saved, saves our custom data.
+     *
+     * @param int $post_id
+     */
+    function save_jobs_meta( $post_id ) {
+
+        // Check if our nonce is set.
+        if ( ! isset( $_POST['global_notice_nonce'] ) ) {
+            return;
+        }
+
+        // Verify that the nonce is valid.
+        if ( ! wp_verify_nonce( $_POST['global_notice_nonce'], 'global_notice_nonce' ) ) {
+            return;
+        }
+
+        // If this is an autosave, our form has not been submitted, so we don't want to do anything.
+        if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+            return;
+        }
+
+        // Check the user's permissions.
+        if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
+
+            if ( ! current_user_can( 'edit_page', $post_id ) ) {
+                return;
+            }
+
+        }
+        else {
+
+            if ( ! current_user_can( 'edit_post', $post_id ) ) {
+                return;
+            }
+        }
+
+        /* OK, it's safe for us to save the data now. */
+
+        // Make sure that it is set.
+        if ( ! isset( $_POST['title-meta'] ) ) {
+            return;
+        }
+
+        // Sanitize user input.
+        $my_data = sanitize_text_field( $_POST['title-meta'] );
+
+        // Update the meta field in the database.
+        update_post_meta( $post_id, '_title-meta', $my_data );
+
+        if ( ! isset( $_POST['email-meta'] ) ) {
+            return;
+        }
+
+        // Sanitize user input.
+        $my_data1 = sanitize_email( $_POST['email-meta'] );
+
+        // Update the meta field in the database.
+        update_post_meta( $post_id, '_email-meta', $my_data1 );
     }
 
-    function custom_post_type() {
-      register_post_type( 'jobs',['public' => true, 'label' => 'Custom-Jobs',
-      'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments')] );
+
+
+
+      function jobs_load_jquery_datepicker() {
+          wp_enqueue_script( 'jquery-ui-datepicker' );
+          wp_enqueue_style( 'jquery-style', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css' );
+      }
+
+
+      function activate() {
+        $this->custom_post_type();
+        flush_rewrite_rules();
+      }
+
+      function deactivate() {
+        flush_rewrite_rules();
+      }
+
+
+      function custom_post_type() {
+        register_post_type( 'jobs', ['public' => true, 'label' => 'Custom Jobs',] );
+      }
+
+
+     function  enqueue() {
+       wp_enqueue_style( 'mypluginstyle', plugins_url( '/assets/mystyle.css', __FILE__) );
+       wp_enqueue_script( 'mypluginscript', plugins_url( '/assets/myscript.js', __FILE__) );
+     }
     }
 
-    function  enqueue() {
-      wp_enqueue_style( 'mypluginstyle', plugins_url( '/assets/mystyle.css', __FILE__) );
-      wp_enqueue_script( 'mypluginscript', plugins_url( '/assets/myscript.js', __FILE__) );
-    }
-  }
-
-  if ( class_exists( 'CutomJobsPlugin' ) ) {
     $cutomJobsPlugin = new CutomJobsPlugin();
     $cutomJobsPlugin->register();
-  }
 
-//plugin_activation
 
-  register_activation_hook(__FILE__ , array( $cutomJobsPlugin, 'activate' ) );
 
-//plugin_deactivation
+  //plugin_activation
 
-  register_deactivation_hook(__FILE__ , array( $cutomJobsPlugin, 'deactivate' ) );
+    register_activation_hook(__FILE__ , array( $cutomJobsPlugin, 'activate' ) );
+
+  //plugin_deactivation
+
+    register_deactivation_hook(__FILE__ , array( $cutomJobsPlugin, 'deactivate' ) );
 
 }
-//uninstall
+
+    if (!function_exists('write_log')) {
+    	function write_log ( $log )  {
+    		if ( true === WP_DEBUG ) {
+    			if ( is_array( $log ) || is_object( $log ) ) {
+    				error_log( print_r( $log, true ) );
+    			} else {
+    				error_log( $log );
+    			}
+    		}
+    	}
+    }
+
+
+
+  //uninstall
